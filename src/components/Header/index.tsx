0723e8f139f77dc80bbac0f24022d52f168e3222
Jose Henrique <jh4832184@gmail.com>
2024-05-16 14:38:25 -0300

    refactor: add new style header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,25 @@
+import { Button } from "../Button";
 import { Link } from "../Link";
 
 export function Header() {
     const links = [
-        {children: "HOME", href: "#"},
-        {children: "ABOUT ME", href: "#"},
-        {children: "PROJECTS", href: "#"},
-        {children: "SERVICES", href: "#"},
-        {children: "EXPERIENCE", href: "#"},
-        {children: "CONTACT", href: "#"},
+        { children: "HOME", href: "#" },
+        { children: "ABOUT ME", href: "#" },
+        { children: "PROJECTS", href: "#" },
+        { children: "SERVICES", href: "#" },
+        { children: "EXPERIENCE", href: "#" },
     ]
-    return ( 
-        <header className="flex justify-center items-center min-h-10">
-           <nav className="space-x-6">
-            {
-                links.map(item=> <Link href={item.href}> {item.children} </Link>)
-            }
-           </nav>
+    return (
+        <header className="flex justify-center items-center h-24">
+            <div className="flex justify-between w-full max-w-screen-xl px-2">
+                <h1 className="text-5xl font-semibold text-cyan-500">JH</h1>
+                <nav className="space-x-8">
+                    {
+                        links.map(item => <Link href={item.href}> {item.children} </Link>)
+                    }
+                    <Button className="font-normal">CONTACT</Button>
+                </nav>
+            </div>
         </header>
-     );
+    );
 }
