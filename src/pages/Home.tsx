eb5fbfe83b4c474e5bba698e2164d30c9e1df0f9
Jose Henrique <jh4832184@gmail.com>
2024-05-20 18:28:21 -0300

    view: add style custom for home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { Button } from "../components/Button";
 export function Home() {
   //<main className="flex-grow flex flex-col max-w-screen-xl mx-auto my-0 bg-red-600">
   return (
-    <section className="flex flex-col items-start py-6">
+    <section className="flex flex-col items-start py-6 home">
       <h1 className="text-gray-300 font-semibold text-3xl">I'm a</h1>
       <div className="space-y-4">
         <h1 className="text-white text-9xl font-semibold font-serif tracking-tight ">Full Stack</h1>
