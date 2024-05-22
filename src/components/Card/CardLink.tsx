b35dda7891534aa687812fed974a1a2fd3626d18
Jose Henrique <jh4832184@gmail.com>
2024-05-22 17:10:59 -0300

    view: add info of projects

--- src/components/Card/CardLink.tsx
+++ src/components/Card/CardLink.tsx
@@ -2,9 +2,9 @@ import { AnchorHTMLAttributes } from "react";
 import { MoveRight } from "lucide-react";
 interface cardLinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {}
 
-function CardLink({href}: cardLinkProps) {
+function CardLink(props: cardLinkProps) {
     return ( 
-        <a className="flex items-center gap-4 text-xs text-gray-300" href={href}>
+        <a target="_blank" className="flex items-center gap-4 text-xs text-gray-300 transition duration-300 hover:bg-blue-950 p-2 pr-4 rounded" {...props}>
             <MoveRight/>
             <p>Read more</p>
         </a>
--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,25 +1,31 @@
 import { Card } from "../components/Card";
 
+interface projectInterface {
+    title: string,
+    description: string,
+    src: string,
+    link: string,
+}
 export function Project() {
-    
-    const projects = [
+
+    const projects : Array<projectInterface> = [
         {
             title: "Task Block",
-            description: "Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks |",
-            src: "img/projects/forum-site.png",
-            link: "google.com",
+            description: "Tired of losing your thoughts and ideas amidst the pile of papers? No more relying on complicated apps full of ads! Online TaskBlock is the perfect solution for those looking for a simple and efficient way to organize their notes. With our virtual notepad, you can create notes with title, description, background color and status.",
+            src: "img/projects/task-block.png",
+            link: "https://github.com/JoseHenrique1/TaskBlock",
         },
         {
-            title: "Task Block",
-            description: "Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks |",
-            src: "img/projects/forum-site.png",
-            link: "google.com",
+            title: "FastNews",
+            description: "News site on various topics: Brazil, Football, Europe and much more. Work through specific themes or see highlights on the home page.",
+            src: "img/projects/fastnews.png",
+            link: "https://github.com/JoseHenrique1/FastNews",
         },
         {
-            title: "Task Block",
-            description: "Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks |",
+            title: "Forum",
+            description: "Vibrant and dynamic online forum where you can connect with people who share your interests. Create topics, join conversations, and explore a world of knowledge and ideas!",
             src: "img/projects/forum-site.png",
-            link: "google.com",
+            link: "https://github.com/JoseHenrique1/Forum/tree/main",
         },
     ]
 
@@ -28,10 +34,10 @@ export function Project() {
             <h1 className="text-white self-center text-4xl">Projects</h1>
             {
                 projects.map(project=>(
-                    <Card.Root>
+                    <Card.Root key={project.title}>
                         <Card.Image layout="horizontal" src={project.src} />
                         <Card.Content title={project.title} description={project.description} >
-                            <Card.Link href="#"/>
+                            <Card.Link href={project.link}/>
                         </Card.Content>
                     </Card.Root>
                 ))
