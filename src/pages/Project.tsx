ccdd6c1a26c543448c763718938d6abab8e8dfc4
Jose Henrique <jh4832184@gmail.com>
2024-05-22 08:55:52 -0300

    refactor: organize imgs of projects and removing containerCard component

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,10 +1,41 @@
-import { ContainerCard } from "../components/ContainerCard";
+import { Card } from "../components/Card";
 
 export function Project() {
+    
+    const projects = [
+        {
+            title: "Task Block",
+            description: "Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks |",
+            src: "img/projects/forum-site.png",
+            link: "google.com",
+        },
+        {
+            title: "Task Block",
+            description: "Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks |",
+            src: "img/projects/forum-site.png",
+            link: "google.com",
+        },
+        {
+            title: "Task Block",
+            description: "Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks | Site manager of tasks |",
+            src: "img/projects/forum-site.png",
+            link: "google.com",
+        },
+    ]
+
     return ( 
         <section id="projects" className="min-h-screen flex flex-col justify-start gap-8">
             <h1 className="text-white self-center text-4xl">Projects</h1>
-            <ContainerCard />
+            {
+                projects.map(project=>(
+                    <Card.Root>
+                        <Card.Image layout="horizontal" src={project.src} />
+                        <Card.Content title={project.title} description={project.description} >
+                            <Card.Link href="#"/>
+                        </Card.Content>
+                    </Card.Root>
+                ))
+            }
         </section>
      );
 }
