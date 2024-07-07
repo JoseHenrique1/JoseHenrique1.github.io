75f18e47d9e788616e7c37761c01633c0bc3a63a
Jose Henrique <jh4832184@gmail.com>
2024-07-07 20:27:37 -0300

    fix: fixing font-size of home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,12 @@ import { Button } from "../components/Button";
 export function Home() {
   //<main className="flex-grow flex flex-col max-w-screen-xl mx-auto my-0 bg-red-600">
   return (
-    <section className="flex flex-col justify-center items-start py-6 home sm:justify-start">
-      <h1 className="text-gray-300 font-semibold text-xl sm:text-2xl md:text-3xl">Eu sou um</h1>
+    <section className="flex flex-col justify-center items-start py-6 home lg:justify-start">
+      <h1 className="text-gray-300 font-semibold text-lg sm:text-2xl md:text-3xl">Eu sou um</h1>
       <div className="space-y-4">
-        <h1 className="text-white font-semibold font-serif tracking-tight text-6xl sm:text-8xl lg:text-9xl">Desenvolvedor</h1>
-        <h1 className="text-white font-semibold font-serif tracking-tight leading-3 text-7xl sm:leading-6 sm:text-8xl lg:leading-10 lg:text-9xl">Software</h1>
-        <h1 className="text-white font-semibold font-serif tracking-tight text-7xl sm:text-8xl lg:text-9xl">
+        <h1 className="text-white font-semibold font-serif tracking-tight text-5xl sm:text-8xl lg:text-9xl">Desenvolvedor</h1>
+        <h1 className="text-white font-semibold font-serif tracking-tight leading-3 text-5xl sm:leading-6 sm:text-8xl lg:leading-10 lg:text-9xl">Software</h1>
+        <h1 className="text-white font-semibold font-serif tracking-tight text-5xl sm:text-8xl lg:text-9xl">
           Full Stack
           <span className="text-cyan-500 font-serif ml-1">.</span>
         </h1>
